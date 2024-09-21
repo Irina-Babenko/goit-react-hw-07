96e1eda1464a983a0e8151b89949a17ee4169ed8
Irina Babenko <babenko8irina@gmail.com>
2024-09-22 02:00:16 +0300

    add css

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,11 +2,13 @@ import { useSelector } from 'react-redux';
 import { selectFilteredContacts } from '../../redux/selectors';
 import Contact from '../Contact/Contact';
 
+import css from './ContactList.module.css';
+
 const ContactList = ({ onDelete }) => {
   const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
-    <ul>
+    <ul className={css.listContainer}>
       {filteredContacts.length === 0 ? (
         <li>No contacts found</li>
       ) : (
